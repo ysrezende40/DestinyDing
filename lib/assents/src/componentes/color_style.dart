d551430a67dd5bfb128c626620856963e781be8a
ysaac <ysaacqueiroz@gmail.com>
2024-05-10 21:16:43 -0300

    cria esquema de cores

---
+++ lib/assents/src/componentes/color_style.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorStyle{
+  static const primary = Color.fromARGB(19, 55, 240, 8);
+  static const secudary = Color.fromARGB(19, 154, 241, 161);
+  static const background = Color.fromARGB(19, 221, 244, 224);
+
+}
